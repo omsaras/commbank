c98c1aa21a0af513f5752c12427892794ce615c1
rchauhanaws <chauhan-rahul@hotmail.com>
2024-01-05 00:36:52 +0000

    quote file modified

---
+++ arithmetic.sh
@@ -0,0 +1,8 @@
+echo "Function to get the sum of three numbers:"
+echo "Enter first number:"
+read firstnumber
+echo "Enter second number:"
+read secondnumber
+echo "Enter third number:"
+read thirdnumber
+echo "Sum of three numbers are: `expr firstnumber + secondnumber + thirdnumber`"
---
+++ quotes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+single='Single Quoted'
+double="Double Quoted"
+echo $single
+echo $double
+#echo -e "Hello \t Welcometo Linux Shell Scripting"
+echo -e "Hello \r Welcome to my channel"
+echo "files been updated"
